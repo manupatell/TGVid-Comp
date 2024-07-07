63aef53c3c326ef976614d65129cca001c28c0e3
Raghu Ram <manumanhothra1618@gmail.com>
2024-07-07 17:47:35 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -8,20 +8,55 @@ class Database:
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.col = self.db.user
+        self.col = self.db.users
 
-   def new_user(self, id):
+    def new_user(self, id):
         return dict(
-            _id = int(id),
-            file_id = None,
-            caption = None,
-            prefix = None,
-            suffix = None,
-            metadata = True,
-            metadata_code = "Telegram : @KPSBots"
+            id=int(id),
+            join_date=datetime.date.today().isoformat(),
+            caption=None,
+            thumbnail=None,
+            ffmpegcode=None,
+            metadata=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @Kdramaland" -metadata author="@Snowball_Official" -metadata:s:s title="Subtitled By :- @Kdramaland" -metadata:s:a title="By :- @Kdramaland" -metadata:s:v title="By:- @Snowball_Official" """,
+            ban_status=dict(
+                is_banned=False,
+                ban_duration=0,
+                banned_on=datetime.date.max.isoformat(),
+                ban_reason=''
+            )
         )
 
-   
+    async def set_caption(self, user_id, caption):
+        await self.col.update_one({'id': int(user_id)}, {'$set': {'caption': caption}})
+
+    async def get_caption(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('caption', None)
+    
+
+    async def set_thumbnail(self, user_id, thumbnail):
+        await self.col.update_one({'id': int(user_id)}, {'$set': {'thumbnail': thumbnail}})
+
+    async def get_thumbnail(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('thumbnail', None)
+    
+
+    async def set_ffmpegcode(self, user_id, ffmpegcode):
+        await self.col.update_one({'id': int(user_id)}, {'$set': {'ffmpegcode': ffmpegcode}})
+
+    async def get_ffmpegcode(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('ffmpegcode', None)
+    
+
+    async def set_metadata(self, user_id, metadata):
+        await self.col.update_one({'id': int(user_id)}, {'$set': {'metadata': metadata}})
+
+    async def get_metadata(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('metadata', None)
+
     async def add_user(self, b, m):
         u = m.from_user
         if not await self.is_user_exist(u.id):
@@ -42,49 +77,39 @@ class Database:
         return all_users
 
     async def delete_user(self, user_id):
-        await self.col.delete_many({'_id': int(user_id)})
+        await self.col.delete_many({'id': int(user_id)})
 
-    async def set_thumbnail(self, id, file_id):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
+    async def remove_ban(self, id):
+        ban_status = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason=''
+        )
+        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
 
-    async def get_thumbnail(self, id):
+    async def ban_user(self, user_id, ban_duration, ban_reason):
+        ban_status = dict(
+            is_banned=True,
+            ban_duration=ban_duration,
+            banned_on=datetime.date.today().isoformat(),
+            ban_reason=ban_reason
+        )
+        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
+
+    async def get_ban_status(self, id):
+        default = dict(
+            is_banned=False,
+            ban_duration=0,
+            banned_on=datetime.date.max.isoformat(),
+            ban_reason=''
+        )
         user = await self.col.find_one({'_id': int(id)})
-        return user.get('file_id', None)
+        return user.get('ban_status', default)
 
-    async def set_caption(self, id, caption):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
-
-    async def get_caption(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('caption', None)
-
-    async def set_prefix(self, id, prefix):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'prefix': prefix}})
-
-    async def get_prefix(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('prefix', None)
-
-    async def set_suffix(self, id, suffix):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'suffix': suffix}})
-
-    async def get_suffix(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('suffix', None)
-
-    async def set_metadata(self, id, bool_meta):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata': bool_meta}})
-
-    async def get_metadata(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('metadata', None)
-
-    async def set_metadata_code(self, id, metadata_code):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata_code': metadata_code}})
-
-    async def get_metadata_code(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('metadata_code', None)
+    async def get_all_banned_users(self):
+        banned_users = self.col.find({'ban_status.is_banned': True})
+        return banned_users
 
 
 db = Database(Config.DB_URL, Config.DB_NAME)
